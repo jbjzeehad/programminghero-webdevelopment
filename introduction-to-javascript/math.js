9e000e6790c3cccd204ac3687f3f68983781e144
jbjzeehad <ilamta.97@gmail.com>
2022-09-13 22:30:32 +0600

    after a long time

---
+++ introduction-to-javascript/math.js
@@ -0,0 +1,37 @@
+// var price = 11;
+// var age = 25;
+// var temperature = 38;
+// var weight = 50;
+// var person = "jbj zeehad";
+// var loction = "Dhaka";
+// var special = 'Pinki';
+// console.log(price + loction);
+
+// var tomatoPrice = 25;
+// tomatoPrice = 30;
+
+// var price1 = 5;
+// var price2 = 4;
+
+// price2 += 5;
+// price1 -= 1;
+// price1 *= 2;
+
+// console.log(price1);
+/*
+var price1 = 31;
+var price2 = 10;
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+var firstName = 10;
+var lastName = "Zeehad";
+console.log(firstName + " " + lastName);
+console.log(typeof price1);*/
+
+/*var frndAge = [14, 25, 26, 5, 8];
+var b = ["a", "e", "i", 'o', 'u'];
+console.log(b.length);
+console.log(b[2]);
+console.log(b.indexOf("e"));
+b.push("gg");
+console.log(b);*/
---
+++ javascriptPractise/PractiseDay.js
@@ -0,0 +1,38 @@
+// var cnt = 0;
+// var textLine = "aske amar mon valo nai"
+// while (cnt < 39) {
+//     console.log(cnt + " " + textLine);
+//     cnt++;
+// }
+// console.log(cnt);
+// for (var i = 58; i <= 98; i++) {
+//     console.log(i);
+// }
+// for (var i = 581; i <= 623; i += 2) {
+//     console.log(i);
+// }
+// for (var i = 30; i <= 86; i++) {
+//     if (i == 44) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// var bookPrice = [100, 54, 58, 220, 189, 185, 205, 256, 125, 149, 289];
+// for (var i = 0; i < bookPrice.length; i++) {
+//     if (bookPrice[i] > 200) {
+//         continue;
+//     }
+//     console.log(bookPrice[i]);
+// }
+// var mobileName = ["Nokia", "iphone", "symphony", "motorola", "realme", "samsung", "sony"];
+// var i = 0;
+// while (i < mobileName.length) {
+//     console.log(mobileName[i]);
+//     i++;
+// }
+
+console.log("Hello World!\n");
+var line = "Hello World!\n";
+console.log(line);
+console.log("Hello World!\n");
