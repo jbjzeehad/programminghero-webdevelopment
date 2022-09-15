65e1546fc322c779b792055c51ad24b6eeb5b339
jbjzeehad <ilamta.97@gmail.com>
2022-09-15 22:35:40 +0600

    peace

---
+++ java-core/recap.js
@@ -0,0 +1,52 @@
+// var nameO = 'Jbj Zeehad';
+
+// console.log(nameO);
+
+// function isGo() {
+//     return 3 + 3;
+// }
+// console.log(isGo());
+
+// function isConvert(inches) {
+//     const feet = inches / 12;
+//     return feet;
+// }
+// const dadafeet = isConvert(45);
+// console.log("Dada Feet", dadafeet);
+
+// function isEven(number) {
+//     if (number % 2 == 0) {
+//         return 'Even';
+//     }
+//     else {
+//         return 'odd';
+//     }
+
+// }
+// console.log(isEven(10));
+
+function getFactorial(value) {
+    let a = 1;
+    for (let i = 0; i < value; value--) {
+
+        a *= value;
+
+    }
+    console.log(a);
+}
+
+// const arrn = [1, 2, 3, 4, 5, 6, 7];
+getFactorial(3);
+// function getsum(value) {
+//     let a = 0;
+//     for (let i = 0; i < arrn.length; i++) {
+//         if (value[i] % 2 == 1) {
+//             a += value[i];
+//         }
+//     }
+//     console.log(a);
+// }
+
+// const arrn = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// getsum(arrn);
+
